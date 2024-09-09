8dae3c774e3dfacc3d9a88893a19888b3c0e0dc3
Ali Haggag <88595616+AliHaggag11@users.noreply.github.com>
2024-09-10 01:59:57 +0300

    manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,19 +41,29 @@ export default function RootLayout<T extends React.ReactNode>({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content="Cashii" />
+        <meta
+          property="og:description"
+          content="Budgeting & Expense Tracking - The Smart Way!"
+        />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/images/618b2b6d-dc2b-4bb2-b25d-8f57fe9aabad.jpg?token=Xz01gONTR9d_kqlVdgHI-UHExJJiDtotTBJTpHJA3uI&height=500&width=500&expires=33261921899"
+        />
+
+        {/* PWA manifest and theme color */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+
+        {/* iOS PWA meta tags */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <head>
-            <meta property="og:title" content="Cashii" />
-            <meta
-              property="og:description"
-              content="Budgeting & Expense Tracking - The Smart Way!"
-            />
-            <meta
-              property="og:image"
-              content="https://opengraph.b-cdn.net/production/images/618b2b6d-dc2b-4bb2-b25d-8f57fe9aabad.jpg?token=Xz01gONTR9d_kqlVdgHI-UHExJJiDtotTBJTpHJA3uI&height=500&width=500&expires=33261921899"
-            />
-          </head>
           {children}
         </ThemeProvider>
       </body>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // Other Next.js config options if needed
+});
 
 export default nextConfig;
